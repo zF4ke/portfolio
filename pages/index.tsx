45d374ce3e3b66b747e0407f9432138c405a8daa
zF4ke <pedrohsilva955@gmail.com>
2022-01-30 19:30:36 +0000

    wrote some stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import fs from "fs";
 import path from "path";
 
 import { GetStaticProps } from "next";
-
 import Head from "next/head";
+import { useRef } from "react";
+
 import Main from "../components/Main";
 import Section from "../components/Section";
-import { useRef } from "react";
+import Emoji from "../components/Emoji";
 
 export const getStaticProps: GetStaticProps = async () => {
   const songsDirectory = path.join(process.cwd(), "public/musics");
@@ -44,69 +45,83 @@ const Home = (props: props) => {
       />
 
       <Section title={"Who Am I"}>
-        <p ref={whoAmISectionRef}>Test</p>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div ref={whoAmISectionRef}>
+          <p>zf4ke@archlinux</p>
+          <br />
+          <p>
+            Hey! My name is <strong>Pedro Silva</strong>, I&apos;m 17 years old,
+            I&apos;m Portuguese <Emoji symbol="🇵🇹" /> and I&apos;m currently in
+            my last year of high school. I started coding since I was 11 years
+            old and I&apos;m particularly interested in Web Development and
+            Cyber Security.
+          </p>
+          <br />
+          <p>
+            When I&apos;m not coding in my bedroom I&apos;m probably watching a
+            movie or just sleeping :)
+          </p>
+        </div>
       </Section>
 
       <Section title={"Skills"}>
-        <p>Sussy</p>
-        <br />
-        <p>
-          Voluptate labore laboris pariatur sunt ex nulla voluptate id cillum.
-          Proident laborum ex exercitation aliqua sunt deserunt proident labore
-          ut. Do voluptate anim sint adipisicing aliqua labore aliquip.
-        </p>
-        <br />
-        <p>
-          Ut amet ad commodo aliqua in enim. Aliquip in sunt adipisicing magna
-          laborum nostrud laborum officia ea deserunt est et. Culpa enim magna
-          dolor aute officia ut est culpa qui pariatur consequat nulla
-          consequat.
-        </p>
+        <div>
+          <p>[...in progress]</p>
+          <br />
+          <p>
+            Voluptate labore laboris pariatur sunt ex nulla voluptate id cillum.
+            Proident laborum ex exercitation aliqua sunt deserunt proident
+            labore ut. Do voluptate anim sint adipisicing aliqua labore aliquip.
+          </p>
+          <br />
+          <p>
+            Ut amet ad commodo aliqua in enim. Aliquip in sunt adipisicing magna
+            laborum nostrud laborum officia ea deserunt est et. Culpa enim magna
+            dolor aute officia ut est culpa qui pariatur consequat nulla
+            consequat.
+          </p>
+        </div>
       </Section>
 
       <Section title={"Projects"}>
-        <p ref={projectsSectionRef}>Oh, Hi my name is Jeniffer</p>
-        <br />
-        <p>
-          Voluptate est consectetur deserunt cillum. Minim nostrud reprehenderit
-          voluptate sit tempor qui. Non labore eiusmod sunt pariatur anim cillum
-          ipsum veniam esse. Lorem id dolore in enim esse veniam qui sint irure
-          elit sint.
-        </p>
-        <br />
-        <p>
-          Sit exercitation nisi quis dolor deserunt reprehenderit. Fugiat duis
-          proident commodo aute veniam. Eiusmod est quis sunt ex proident cillum
-          consectetur sunt minim. Non qui elit velit est velit in exercitation.
-          Do Lorem in minim laboris amet. Reprehenderit do velit ut Lorem nulla
-          in non officia quis veniam. Consectetur sint excepteur minim in dolore
-          laborum. Amet veniam cupidatat amet do in eu nulla ut. Consequat
-          nostrud consectetur ullamco exercitation.
-        </p>
+        <div ref={projectsSectionRef}>
+          <p>{"{ github.com/zf4ke }"}</p>
+          <br />
+          <p>
+            In progress... voluptate est consectetur deserunt cillum. Minim
+            nostrud reprehenderit voluptate sit tempor qui. Non labore eiusmod
+            sunt pariatur anim cillum ipsum veniam esse. Lorem id dolore in enim
+            esse veniam qui sint irure elit sint.
+          </p>
+          <br />
+          <p>
+            Sit exercitation nisi quis dolor deserunt reprehenderit. Fugiat duis
+            proident commodo aute veniam. Eiusmod est quis sunt ex proident
+            cillum consectetur sunt minim. Non qui elit velit est velit in
+            exercitation. Do Lorem in minim laboris amet. Reprehenderit do velit
+            ut Lorem nulla in non officia quis veniam. Consectetur sint
+            excepteur minim in dolore laborum. Amet veniam cupidatat amet do in
+            eu nulla ut. Consequat nostrud consectetur ullamco exercitation.
+          </p>
+        </div>
       </Section>
 
       <Section title={"Contact"}>
-        <p ref={contactSectionRef}>O.o o.o o.O</p>
-        <br />
-        <p>
-          Aliquip adipisicing consequat aliquip anim irure nostrud veniam ipsum
-          tempor. Ipsum sunt amet officia officia id id sunt aliqua exercitation
-          laborum ad cupidatat sit eiusmod.
-        </p>
-        <br />
-        <button className="bg-violet-500 hover:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700 transitions-all duration-100 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white">
-          Click Me!
-        </button>
+        <div ref={contactSectionRef}>
+          <p>O.o ~ o.o ~ o.O</p>
+          <br />
+          <ul>
+            <li>github: @zf4ke</li>
+            <li>youtube: zFake</li>
+            <li>linkedin: @zf4ke</li>
+            <li>twitter: @zF4ked</li>
+            <li>instagram: @zF4ked</li>
+          </ul>
+          <p>...</p>
+          <br />
+          <button className="bg-violet-500 hover:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700 transitions-all duration-100 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white">
+            Click Me!
+          </button>
+        </div>
       </Section>
 
       <div className="mt-32"></div>
